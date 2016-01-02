306d5ee5829f258be1859bf25a3ca726f2c59d82
balboni <brian@brianbalboni.com>
2016-01-02 11:32:13 -0600

    Initial Commit

---
+++ ArrayStockCalculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArrayStockCalculator
+//
+//  Created by Brian Balboni on 1/2/16.
+//  Copyright © 2016 Brian Balboni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
